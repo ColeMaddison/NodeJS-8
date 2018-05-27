603ef00b08cebfc10953f621d49e8402a8ed7d9d
Dmitry <dimonas92@gmail.com>
2018-05-28 01:35:03 +0300

    partial search by req params

--- controllers.js
+++ controllers.js
@@ -2,6 +2,8 @@
 
 const fs = require('fs');
 const path = require('path');
+const querystring = require('querystring');
+const url = require('url');
 
 let dir = path.join(__dirname, 'public/img');
 
@@ -26,7 +28,54 @@ exports.ejsRender = (req, res)=>{
 
 // show all saved messages
 exports.showMessages = (req, res)=>{
-    res.status(200).json({data: req.app.locals.messages});
+    let query = url.parse(req.url).query;
+    let queryObj = querystring.parse(query);
+    let db = req.app.locals.messages;
+    let searchRes = [];
+
+    // check if query keys are in database
+    // if(query){
+    //     for(let item in queryObj){
+    //         if(!(item === "endAt" || item === "username")){
+    //             res.status(404).send("Bad query1");
+    //         } else {
+    //             // get messages from db
+    //             for(let dbItem of db){
+    //                 if(queryObj[item] === dbItem[item]){
+    //                     searchRes.push(dbItem);
+    //                 } else {
+    //                     res.status(404).send("Bad query");
+    //                 }
+    //             }
+    //             continue;
+    //         }
+    //     }   
+    //     res.json(searchRes);
+    // } else {
+    //     res.status(200).json({data: db});
+    // }
+    
+    if(query){
+        for(let dbItem of db){
+            for(let item in queryObj){
+                if(!(item === "endAt" || item === "username")){
+                    res.status(404).send("Bad qquery1");
+                } else {
+                    if(queryObj[item] === dbItem[item]){
+                        continue;
+                    } else {
+                        console.log(db.length);
+                        // break;
+                        // res.status(404).end("Bad query");
+                    }
+                }
+            }
+            searchRes.push(dbItem);
+        }
+        res.json(searchRes);
+    } else {
+        res.status(200).json({data: db});
+    }
 };
 
 // save a message
@@ -51,5 +100,9 @@ exports.saveMessage = (req, res)=>{
     }, resMessage.show * 1000);
 
     res.send('OK');
+};
+
+exports.handler404 = (req, res, next) => {
+    res.status('404').end("Not Found");
 };
 
--- router.js
+++ router.js
@@ -11,4 +11,6 @@ router.post('/message', mdl.checkMessage, mdl.setEndDate, ctrl.saveMessage);
 
 router.get('/messages', ctrl.showMessages);
 
+router.get('/*', ctrl.handler404);
+
 module.exports = router;
